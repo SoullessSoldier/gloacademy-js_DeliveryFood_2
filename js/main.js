df6b3d1ff9983dad3800b340a3317f27bd594b60
SoullessSoldier <48070769+SoullessSoldier@users.noreply.github.com>
2021-11-06 09:06:37 +0200

    add Day1

---
+++ js/main.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const buttonAuth = document.querySelector('.button-auth'),
+    modalAuth = document.querySelector('.modal-auth'),
+    closeAuth = document.querySelector('.close-auth'),
+    logInForm = document.getElementById('logInForm'),
+    inputLogin = document.getElementById('login'),
+    inputPassword = document.getElementById('password'),
+    userName = document.querySelector('.user-name'),
+    buttonOut = document.querySelector('.button-out');
+    
+
+
+const login = (user) => {
+    modalAuth.style.display = 'none';    
+    buttonAuth.style.display = 'none';
+    buttonOut.style.display = 'block';
+    userName.textContent = user instanceof FormData ? user.get('login') : user.login;
+    userName.style.display = 'block';
+};
+
+const logout = () => {
+    userName.textContent = '';
+    userName.style.display = 'none';
+    buttonOut.style.display = 'none';
+    buttonAuth.style.display = 'block';
+    localStorage.removeItem('user');
+};
+    
+buttonAuth.addEventListener('click', (e) => {
+    modalAuth.style.display = 'flex';
+});
+
+closeAuth.addEventListener('click', (e) => {
+    modalAuth.style.display = 'none';
+});
+
+buttonOut.addEventListener('click', (e) => {
+    logout();
+});
+
+logInForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(logInForm);
+    if(formData.get('login')){
+        const user = {
+            login: formData.get('login'),
+            password: formData.get('password')
+        };
+        
+        localStorage.setItem('user', JSON.stringify(user));
+        login(formData);
+    } else {
+        [inputLogin, inputPassword].forEach(element => element.value = '');
+        alert('Логин не введен!');
+    }
+
+});
+
+if (localStorage.getItem('user')){
+    login(JSON.parse(localStorage.getItem('user')));
+}
